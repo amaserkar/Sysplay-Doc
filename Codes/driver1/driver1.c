0b58cd4c1d11ec352fed92f332330494f7c859f2
amaserkar <110374441+amaserkar@users.noreply.github.com>
2022-08-04 11:05:35 +0530

    Create driver1.c

---
+++ Codes/driver1/driver1.c
@@ -0,0 +1,28 @@
+/*
+Sysplay Doc Ex 1
+Our First Driver
+*/
+
+#include <linux/init.h> // contains macros for module initialisation
+#include <linux/module.h> // includes the module related data structures
+
+int driver1_init(void)// Constructor - initialisation
+{ // called during loading of module
+  printk(KERN_ALERT "Hello. Inside the %s function\n",__FUNCTION__);
+  //printk prints to /var/log/syslog
+  return 0;
+}
+
+void driver1_exit(void) //Destructor - clean up
+{ // called during unloading
+  printk(KERN_ALERT "Goodbye. Inside the %s function\n",__FUNCTION__);
+  //KERN_ALERT is a "log level" which specifies the importance of the message.
+}
+
+module_init(driver1_init);
+module_exit(driver1_exit);
+//Macros to specify the init and exit functions
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Akshay Aserkar");
+//Macros for module related information
