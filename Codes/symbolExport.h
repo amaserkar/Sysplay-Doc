0bc3cfed81268e09b4d6171455ddb95ea95b019a
amaserkar <110374441+amaserkar@users.noreply.github.com>
2022-08-25 12:08:18 +0530

    Update symbolExport.h

--- Codes/symbolExport.h
+++ Codes/symbolExport.h
@@ -7,6 +7,11 @@
 #include <linux/device.h> // for class related definitions
 extern struct class *p_class1; // pointer to class
 extern struct class *get_class1(void);
+/*
+Symbols are exported by the EXPORT_SYMBOL() functions, which create
+a structure and add it to the kernel symbol table and other tables.
+The symbol can then be accessed throughout the kernel space.
+*/
 
 #endif
 #endif
